b7409892d3d5b138002f8636906a6c8370e84c38
main() <main@ehvag.de>
2017-06-06 05:35:06 +0200

    cleanups

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,18 @@
 extern crate libc;
 
 mod sys;
-pub mod futex;
-pub mod rwfutex;
-pub mod rwfutex2;
-pub mod rwfutex3;
-pub mod rwfutex4;
+mod futex;
+// legacy versions:
+//mod rwfutex;
+//mod rwfutex2;
+//mod rwfutex3;
+mod rwfutex4;
 
-pub type Futex = futex::Futex;
-pub type RwFutex = rwfutex3::RwFutex2;
+pub use futex::Futex as Mutex;
+pub use rwfutex4::RwFutex2 as RwLock;
 
 #[cfg(test)]
+#[allow(deprecated)]
 mod tests {
     use std::thread;
     use std::sync::Arc;
@@ -19,11 +21,11 @@ mod tests {
     
     #[test]
     fn mutex() {
-        let futex = Arc::new(Futex::new());
+        let futex = Arc::new(Mutex::new());
         let futex2 = futex.clone();
         futex.acquire();
         thread::spawn(move || {
-            thread::sleep_ms(1000);
+            thread::sleep_ms(100);
             futex2.release();
         }).join().unwrap();
         futex.acquire();
@@ -33,7 +35,7 @@ mod tests {
     #[test]
     fn rwlock() {
         println!("lul");
-        let futex = Arc::new(RwFutex::new());
+        let futex = Arc::new(RwLock::new());
         let futex2 = futex.clone();
         futex.acquire_read();
         futex.acquire_read();
@@ -41,10 +43,10 @@ mod tests {
         thread::spawn(move || {
             futex2.acquire_read();
             futex2.release_read();
-            let resv = futex2.acquire_write();
-            thread::sleep_ms(1000);
-            futex2.release_write(resv);
-        }); //.join().unwrap();
+            futex2.acquire_write();
+            thread::sleep_ms(100);
+            futex2.release_write();
+        });
         futex.release_read();
         futex.release_read();
         futex.release_read();
